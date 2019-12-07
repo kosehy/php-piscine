3b656f155d8209af666c5f60c0cd1e33625cace3
kosehy <kosehy@gmail.com>
2019-12-06 20:21:46 -0800

    v15. add day03 til ex02

---
+++ day03/ex01/nodejs-info.js
@@ -0,0 +1,35 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   app.js                                             :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: sko <marvin@42.fr>                         +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/12/06 14:37:06 by sko               #+#    #+#             //
+//   Updated: 2019/12/06 14:37:08 by sko              ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+// http: https://www.npmjs.com/package/http
+// nodeInfo: https://www.npmjs.com/package/nodeinfo
+// library for accessing or displaying NodeJS/NPN/System information
+// kind of like phpinfo()
+// Node.js HTTP Module : https://www.w3schools.com/nodejs/nodejs_http.asp
+
+var http     = require('http');
+var nodeInfo = require('nodejs-info');
+
+function onRequest(request, response) {
+    // write the header of response
+    // if the status code is 200, all is OK
+    // if 404, then the page is not found
+    // after response.writeHead, response.end should be followed
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(nodeInfo(request));
+}
+// listen the 8080 port
+http.createServer(onRequest).listen(8080);
+
+// when succesfully connected to server, keep show the below server status and port information
+console.log('Server Status: [RUNNING]');
+console.log('Server listening on port \'8080\'.');
---
+++ day03/ex02/print_get.js
@@ -0,0 +1,39 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   print_get.js                                       :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: sko <marvin@42.fr>                         +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/12/06 15:01:16 by sko               #+#    #+#             //
+//   Updated: 2019/12/06 15:01:18 by sko              ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+// ulrparser :  https://www.npmjs.com/package/urlparser
+// provides with an url parser that deconstructs an url into a managable
+// object and back to a string
+
+var http = require('http');
+var parse_url = require('urlparser');
+
+function onRequest(request, response) {
+    response.writeHead(200, { "Context-Type": "text/plain" });
+    try {
+        // parse the params in to managable object and store to key variable
+        var params = parse_url.parse(request.url).query.params;
+        for (var key in params) {
+            // if param is exists, print out the both key and params
+            if (params.hasOwnProperty(key))
+                response.write(`${key}: ${params[key]}\n`);
+        }
+        response.end();
+    } catch (err) {
+        response.end('add variables at the end of link!!');
+    }
+}
+
+http.createServer(onRequest).listen(8080);
+
+console.log("Server Status: [RUNNING]");
+console.log("Server listening at '127.0.0.1' on port '8080'.");
